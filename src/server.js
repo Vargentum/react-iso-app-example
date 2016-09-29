83de9b02cb989ebc95c7d8cfd6047dc525d08dee
Vargentum <vargentum@gmail.com>
2016-09-29 09:06:14 +0300

    add Server rendering

--- src/server.js
+++ src/server.js
@@ -1,13 +1,41 @@
 import express  from 'express';
+import React      from 'react';
+import ReactDOMServer   from 'react-dom/server';
+import App        from 'components/App';
 
 const app = express();
 
 app.use((req, res) => {
-  res.end('<p>Hello World!</p>');
+  const componentHTML = ReactDOMServer.renderToString(<App />)
+  res.end(renderHTML(componentHTML));
 });
 
+const assetUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:8050' : '/';
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on: ${PORT}`);
 });
+
+
+/* -----------------------------
+  helper to provide basic page markup
+----------------------------- */
+
+function renderHTML(componentHTML) {
+  return `
+    <!DOCTYPE html>
+      <html>
+      <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Hello React</title>
+          <link rel="stylesheet" href="${assetUrl}/public/assets/styles.css">
+      </head>
+      <body>
+        <div id="react-view">${componentHTML}</div>
+        <script type="application/javascript" src="${assetUrl}/public/assets/bundle.js"></script>
+      </body>
+    </html>
+  `;
+}
