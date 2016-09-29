302055c2d37593ba5251658ad625f2e01a3201f8
Vargentum <vargentum@gmail.com>
2016-09-29 08:38:34 +0300

    add ESLint

--- webpack.config.js
+++ webpack.config.js
@@ -60,12 +60,13 @@ module.exports = {
       { test: /\.png$/, loader: 'url-loader?limit=10000&mimetype=image/png' },
       { test: /\.svg/, loader: 'url-loader?limit=26000&mimetype=image/svg+xml' },
       { test: /\.(woff|woff2|ttf|eot)/, loader: 'url-loader?limit=1' },
-      { test: /\.jsx?$/, loader: 'babel', exclude: [/node_modules/, /public/] },
+      { test: /\.jsx?$/, loader: 'babel!eslint-loader', exclude: [/node_modules/, /public/] },
       { test: /\.json$/, loader: 'json-loader' },
     ]
   },
   devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : null,
   devServer: {
     headers: { 'Access-Control-Allow-Origin': '*' }
-  }
+  },
+  eslint: {configFile: '.eslintrc'}
 };
